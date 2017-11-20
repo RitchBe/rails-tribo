920e786a1371d5941dde03f6c654da334c282942
Richard Houet <itchy.ooc@gmail.com>
2017-11-20 17:38:00 +0000

    Pundit fixed

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,20 +1,25 @@
 class LocationsController < ApplicationController
-
-  before_action :set_location, only [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
 
   def index
     @location = Location.all
+    authorize @location
   end
 
   def new
     @user = User.find(current_user.id)
 
     @location = Location.new
+    authorize @location
+
   end
 
   def create
     @user = User.find(current_user.id)
     @location = @user.locations.new(location_params)
+    authorize @location
+
     if @location.save
       redirect_to user_path(current_user.id)
     else
@@ -26,17 +31,22 @@ class LocationsController < ApplicationController
 
   def show
     @user = User.find(current_user.id)
+    authorize @location
   end
 
   def edit
     @user = User.find(current_user.id)
 
     @location = Location.find(params[:id])
+    authorize @location
+
   end
 
   def update
     @location = Location.find(params[:id])
     @location.update(location_params)
+    authorize @location
+
 
     redirect_to :back
   end
@@ -44,6 +54,8 @@ class LocationsController < ApplicationController
   def destroy
     @location = Location.find(params[:id])
     @location.destroy
+    authorize @location
+
     redirect_to user_path(current_user.id)
   end
 
