f8b9b4ceaacdc8b0a7ec075ce324f02e04ee8f90
gon.fervic <gon.fervic@gmail.com>
2017-11-20 15:20:29 +0000

    validate location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,12 @@
 class Location < ApplicationRecord
   belongs_to :user_id
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :address, presence: true, uniqueness: true
+  validates :phone, presence:true
+  validates :description, length: { maximum: 500 }
+  validates :rules, length: { maximum: 400 }
+
 end
+
