fe464517c33dfa733712936b297e76e1e906a59d
hugo rosa <hugo.rosa@museek.io>
2017-11-20 19:08:13 +0000

    welcome mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :locations
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
