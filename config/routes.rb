e0c17921eadaf7d67be6f419ffc5cc0e86e18c93
Richard Houet <itchy.ooc@gmail.com>
2017-11-20 16:43:02 +0000

    modifie routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :path => 'accounts'
   resources :users, only: [:show]
   root to: 'pages#home'
+  resources :locations
   resources :users do
-    resources :locations do
-      resources :bookings
-    end
-  end
+    resources :locations, only: [:show, :index]
+
+  resources :bookings
     end
 
 
