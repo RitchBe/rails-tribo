0d5e911fb90f2e240633513f9883bfab2b54c91e
gon.fervic <gon.fervic@gmail.com>
2017-11-20 19:47:36 +0000

    change user model and initializer. Also routes and schema by running the new migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => 'accounts'
+  devise_for :users, :path => 'accounts',
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
   root to: 'pages#home'
   resources :locations
