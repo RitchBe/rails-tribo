7df71b3c014e76168af8e99c40bbd7115bab8e88
gon.fervic <gon.fervic@gmail.com>
2017-11-21 14:37:11 +0000

    added pic seeds and change location to accept attachinary

--- app/models/location.rb
+++ app/models/location.rb
@@ -10,6 +10,6 @@ class Location < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
-  has_attachments :photos, maximum: 5
+  has_attachments :photos, maximum: 3
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -89,8 +89,16 @@ end
       rules: "No smoking inside",
       name: Faker::Space.planet
       )
-end
 
+      urls = [
+          'https://picsum.photos/200/300/?random',
+          'https://picsum.photos/200/300/?random'
+          'https://picsum.photos/200/300/?random'
+      ]
+
+      product.photo_urls = urls # Multi-upload happens here
+      product.save!
+end
 
 puts "create 15 fake booking"
 
