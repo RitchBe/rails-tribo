f4197d3f2bc98358a6f8391caf9f3935c9ec4a1a
Richard Houet <itchy.ooc@gmail.com>
2017-11-22 21:04:39 +0000

    Chat working -> JAVASCRIPT FROm NOW

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about', to: 'pages#about'
   resources :locations do
-    resources :messages do
-      resources :users
-    end
-    resources :users do
-      resources :messages
-    end
+    resources :messages
   end
   resources :users do
     resources :locations, only: [:show, :index, :create, :update]
