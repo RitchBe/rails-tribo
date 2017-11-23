f7c58594ac246d7a916d1fec66bdf9bb3f6ac69b
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-23 12:31:38 +0000

    landing rough version done

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require "sprockets/railtie"
 require "attachinary/orm/active_record"
 
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :path => 'accounts',
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: [:show]
-  root to: 'pages#home'
+  root to: 'pages#landing'
   get '/about', to: 'pages#about'
   resources :locations
   resources :users do
