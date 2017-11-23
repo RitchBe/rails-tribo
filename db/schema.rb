de3ff9d013c74cb15756010f75d2282879d12e03
hugo rosa <hugo.rosa@museek.io>
2017-11-23 16:21:19 +0000

    add Location View Feature Checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123085326) do
+ActiveRecord::Schema.define(version: 20171123155127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20171123085326) do
     t.integer "rate_by_hour"
     t.integer "rate_by_day"
     t.integer "rate_by_week"
-    t.string "features"
+    t.string "features", default: [], array: true
     t.string "tags"
     t.text "rules"
     t.datetime "created_at", null: false
