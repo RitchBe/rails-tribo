e1ae5fea4ff1052f4c227d681c1c8625ef844eef
hugo rosa <hugo.rosa@museek.io>
2017-11-24 15:01:06 +0000

    landing some changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
-  def home
-  end
+	skip_before_action :authenticate_user!
+	def home
+	end
+
+	def landing
+		@locations = Location.all
+	end
+
 end
