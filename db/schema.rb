e40f91956f40f259cc69a412fd6adca14b1e084f
gon.fervic <gon.fervic@gmail.com>
2017-11-24 02:17:18 +0000

    added migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171123120826) do
+ActiveRecord::Schema.define(version: 20171123155127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
