4c0385f82deeb8e02a18806c2ed1b7bab0c43e3a
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-25 15:33:37 +0000

    booking fixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'sessions',
    omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
    resources :bookings, only: [:index]
   end
 
