e61f8f589067a491c2bfba80d3802c9b9e71deaf
Richard Houet <itchy.ooc@gmail.com>
2017-11-25 13:53:18 +0000

    ADd smileys to chat (bundle install)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,7 @@
 //= require turbolinks
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
+//= require rails_emoji_picker
 //= require attachinary
 //= require_tree ./app
 
