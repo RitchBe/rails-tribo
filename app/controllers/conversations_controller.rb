4c8f2c17d09505f800e3d544074349a06d71be40
Richard Houet <itchy.ooc@gmail.com>
2017-11-25 20:26:12 +0000

    message private

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,9 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
+    @conversations = @conversations.order('updated_at DESC')
     @users = User.where.not(id: current_user.id)
+    @private_messages = PrivateMessage.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
   end
 
 
@@ -16,8 +18,12 @@ class ConversationsController < ApplicationController
     redirect_to conversation_private_messages_path(@conversation)
   end
 
+  def delete
+    Conversation.find(params[:id]).delete
+  end
+
   private
     def conversation_params
-      params.permit(:sender_id, :receiver_id)
+      params.permit(:sender_id, :receiver_id, :id)
     end
 end
