77893582aeb085afe9cfefb42a3a0cea297069ed
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-27 14:31:58 +0000

    update on show bookings for the user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-
+Message.destroy_all
+Conversation.destroy_all
 Booking.destroy_all
 Location.destroy_all
 User.destroy_all
 
 puts "DESTROY DATABASE!!!"
 
-FEATURES = ["High-speed Wi-Fi", "Coffee&Tea", "FridayBeers", "Printers", "Outlets", "Host", "Meeting Rooms","Bicycle Lodge",
+FEATURES = ["High-speed Wi-Fi", "Coffee&Tea", "Friday Beers", "Printers", "Outlets", "Host", "Meeting Rooms","Bicycle Parking",
 "Events", "Whiteboards", "Office Supplies"]
 
 TAGS = ["#Zen", "#Trendy", "#Vintage", "#Industrial", "#Hipster", "#Design", "#Code", "#Marketing", "#City Centre", "#Chill",
   "#Nice Views", "#Music", "#Social", "#Roomy", "#Inspiring", "#Dimly lighted", "#Outside area"]
 
-RULES = ["Smoking not allowed", "Pets allowed","No ??", "Instant Booking", "Early Dinner Access", "Networking",
-"Restaurant Disscount"]
+RULES = ["Smoking not allowed", "Pets allowed","No ??", "Early Dinner Access", "Networking",
+"Restaurant Discount"]
 
 ACTIVITY = ["Web developper", "Designer", "Data analysist", "Video editor", "Bussiness Analyst", "Digital Marketing Specialist"]
 
@@ -56,7 +57,7 @@ end
 
 puts "Creating 17 real locations..."
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "The Good The bad and the Ugly",
   email: "info@obomomaueovilao.com",
@@ -77,13 +78,13 @@ restaurant = Location.create({
   latitude:  38.707362,
   longitude: -9.143454
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://lookmag.pt/wp-content/uploads/2017/02/TOPO-Clube1.jpg',
   'http://www.evenues.com/GetImage.ashx?PhotoId=19991'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "The Insólito",
   email: "insolito@hotmail.com",
@@ -103,13 +104,13 @@ restaurant = Location.create({
   latitude:  38.714978,
   longitude: -9.144686
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://images-cdn.impresa.pt/bcbm/2016-05-18-22201908611_33a3120640_b.jpg/3x2/mw-1240',
   'http://upmagazine-tap.com/app/uploads/2014/03/homelisbon.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Shanghai Penthouse",
   email: "penthouse@gmail.com",
@@ -127,14 +128,14 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 21,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://retaildesignblog.net/wp-content/uploads/2016/12/WeWork-Yanping-Lu-Coworking-Office-by-Linehouse-Shanghai-China05-1.jpg',
   'https://www.hospitalitydesign.com/wp-content/uploads/Linehouse_Herschel.png',
   'https://i.pinimg.com/originals/cd/40/7a/cd407ad0eb29dd583f86001c2aa61cb6.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Ritch Beee in da House",
   email: "ritchbeindahouse@gmail.com",
@@ -147,19 +148,19 @@ restaurant = Location.create({
   rate_by_day: 11,
   rate_by_week: 35,
   features: FEATURES.sample(4),
-  tags: TAGS.sample(3),,
+  tags: TAGS.sample(3),
   rules: RULES.sample(3),
   open_hour: 9,
   close_hour: 21,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://craigbaute.com/wp-content/uploads/2014/06/20141010__20141013_C1_FE13SMCOWORKp1.jpg',
   'http://lisboacool.com/sites/default/files/styles/ny_article_horizontal__w720xh480_watermark/public/lisboa_cool_liberdade22903.jpg?itok=Gjuv05CU',
   'http://cdn.maxima.pt/images/2017-10/img_974x632$2017_10_10_14_22_33_328688.png'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Casa Independente",
   email: "casaindependente@gmail.com",
@@ -179,15 +180,15 @@ restaurant = Location.create({
   latitude:  38.720856,
   longitude: -9.134657
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://www.agendalx.pt/sites/default/files/evento/casaind.jpg',
   'http://www.insidelisbon.com/wp-content/uploads/2016/11/IL_nightlife_drink_casa-independente.jpg',
   'http://cdn.maxima.pt/images/2017-10/img_974x632$2017_10_10_14_22_33_328688.png',
   'https://cdn.betakit.com/wp-content/uploads/2016/08/rndmwrk-web.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "GCoding Station",
   email: "coding@gmail.com",
@@ -205,15 +206,15 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 20,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://i.pinimg.com/564x/a9/c5/48/a9c548325bc4ff4c39071877d10a52b7.jpg',
   'https://admin.citiesintransition.eu/wp-content/uploads/2015/01/Liberdade-229-Coworking-in-Lisbon-1.jpg',
   'https://admin.citiesintransition.eu/wp-content/uploads/2015/01/Liberdade-229-Coworking-in-Lisbon.jpg',
   'http://barcelonanavigator.i5me33nlgv.maxcdn-edge.com/wp-content/uploads/2015/05/Felisa-Coworking-Barcelona.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Clube Ferroviário",
   email: "clubf@gmail.com",
@@ -233,14 +234,14 @@ restaurant = Location.create({
   latitude: 38.718354,
   longitude: -9.118477
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://upmagazine-tap.com/app/uploads/2011/06/24hrs4.jpg',
   'https://www.wedemain.fr/photo/art/default/5788848-8629890.jpg?v=1378821161',
   'https://media-cdn.tripadvisor.com/media/photo-s/08/b0/75/5e/scenes-from-the-clube.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Shubaduba",
   email: "ashubaduba@gmail.com",
@@ -259,15 +260,15 @@ restaurant = Location.create({
   latitude: 38.715034,
   longitude: -9.137011
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://www.coworkcentral.pt/media/images/services/private-rooms.jpg',
   'http://blog.rumbo.pt/wp-content/uploads/sites/16/2017/02/coworkcentral-coworking-lisboa-1024x512.jpg',
   'https://static1.squarespace.com/static/52fcc48ce4b03f845db4aa7f/52fdf05be4b0feb85ec3644f/56d82927b654f9f9ecd0096d/1457008498328/?format=1500w',
   'http://res.cloudinary.com/dzxgxofdh/image/upload/c_scale,q_70,w_800/v1504281616/workhub%20gallery/YellowSavages-Workhub-17.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Red Frog",
   email: "redfrog@gmail.com",
@@ -286,15 +287,15 @@ restaurant = Location.create({
   latitude: 38.715035,
   longitude: -9.137011
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://s3cdn.observador.pt/wp-content/uploads/2015/05/redfrogtp15052015-31.jpg',
   'https://cdn.londonandpartners.com/asset/e9a69784717d5f540e4a77fbe7b0c2e3.jpg',
   'https://www.google.pt/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwibue7jv9rXAhUD7RQKHUbMDl0QjBwIBA&url=https%3A%2F%2Fwww.quintessentially.com%2Fmedia%2F960x0%2F558ab241094c3.jpg&psig=AOvVaw29C-WCB1K6pnt6LsbHQ8lH&ust=1511725780293595',
   'https://www.google.pt/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwiWif2Qy9rXAhUCxxQKHVXwD1MQjBwIBA&url=http%3A%2F%2Fthetaste.ie%2Fwp%2Fwp-content%2Fuploads%2F2016%2F04%2FMarks-bar-2.jpg&psig=AOvVaw29C-WCB1K6pnt6LsbHQ8lH&ust=1511725780293595'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
  user_id: User.all.sample.id,
  name: "Double9",
  email: "double9@gmail.com",
@@ -313,15 +314,15 @@ restaurant = Location.create({
  latitude: 38.712366 ,
  longitude: -9.142971
  })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://www.9-hotel-mercy-lisbon.pt/imgcache/f4dc544d8690c5a5368f30ad0a45f81b_w1138_h488_cp.jpg',
   'https://mb.web.sapo.io/3999df53d6bc1d2307577b264a62f7c51ca4d2f6.jpg',
   'https://d1u4v68pfiv879.cloudfront.net/media/13002/bar-double9_chiado_lisbon-shopping.jpg',
 ]
-restaurant.save
+location.save
 
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Cave 23",
   email: "cave239@gmail.com",
@@ -340,14 +341,14 @@ restaurant = Location.create({
   latitude:  38.718040,
   longitude: -9.140064
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://static.savoteur.com/uploads/content_image/image/2616/Cave23.jpg',
   'https://u.tfstatic.com/restaurant_photos/337/295337/169/612/cave-23-vista-do-interior-4fffc.jpg',
   'http://lisboa.convida.pt/images/POIs/Terraco23-ConVida2017-010.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Edwards Place",
   email: "eduardo@love.com",
@@ -365,14 +366,14 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 17,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://static.wixstatic.com/media/441a2a_32584af7a82d498789fd8ecd072d68da~mv2_d_3976_2651_s_4_2.jpg/v1/fill/w_636,h_424,al_c,q_90,usm_0.66_1.00_0.01/441a2a_32584af7a82d498789fd8ecd072d68da~mv2_d_3976_2651_s_4_2.webp',
   'https://static.wixstatic.com/media/441a2a_cf5d00f42cac4b0eaa9dca1a810086dd~mv2_d_4080_2720_s_4_2.jpg/v1/fill/w_636,h_424,al_c,q_90,usm_0.66_1.00_0.01/441a2a_cf5d00f42cac4b0eaa9dca1a810086dd~mv2_d_4080_2720_s_4_2.webp',
   'https://www.billboard.com/files/media/03-Mass-Appeal-office-space-2017-billboard-embed.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Park",
   email: "park@gmail.com",
@@ -390,14 +391,14 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 17,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://4.bp.blogspot.com/-r0rcHrhdWa8/UrawkIduvHI/AAAAAAAAARI/O6HZp5opbnk/s1600/IMG_8265.JPG',
   'https://lifecooler.com/files/registos/imagens/437845/269020.jpg',
   'http://blog.secretplaces.com/wp-content/uploads/2016/07/Park2.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "IsaYa Lounge",
   email: "Isahias@yahoo.com",
@@ -415,14 +416,14 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 20,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://cdn.wework.com/locations/image/11849c9a-4ebb-11e6-a56f-0a488af3e541/charging-bull-office-space-20.jpg?auto=compress%2Cformat&w=1200&h=600&fit=crop',
   'https://www.homepolish.com/wp-content/uploads/Homepolish-3632-decorating-3aa8613a.jpeg',
   'https://comunalcoworking.com/wp-content/uploads/2017/06/benavidesmiraflores-benavides_DSC1079-600x600_c.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Mikes Place",
   email: "mike@gmail.com",
@@ -439,15 +440,15 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 19,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://blog.liquidspace.com/wp-content/uploads/2017/05/7ef60e93-ba4a-4db0-82d7-f4b53714b236.jpeg',
   'https://officesnapshots.com/wp-content/uploads/2015/06/wework-nyc-office-design-1-700x467.jpg',
   'https://i.pinimg.com/originals/82/44/04/8244040d6eaeb66f0f24bd40ff1d4d0e.jpg'
 ]
-restaurant.save
+location.save
 
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "#MOIST",
   email: "moist@gmail.com",
@@ -466,14 +467,14 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 21,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'http://esdipberlin.com/wp-content/uploads/11080623_684963288313799_3898966742149368400_o.jpg',
   'https://artconnect.s3-eu-west-1.amazonaws.com/attachments/87414/original.jpg?1423259056',
   'https://s3-eu-west-1.amazonaws.com/hubblehq/hubble/uploads/wyswigy/greenhouse-coworking.jpg'
 ]
-restaurant.save
+location.save
 
-restaurant = Location.create({
+location = Location.create({
   user_id: User.all.sample.id,
   name: "Workon",
   email: "peter11@gmail.com",
@@ -491,12 +492,12 @@ restaurant = Location.create({
   open_hour: 9,
   close_hour: 21,
   })
-restaurant.photo_urls = [
+location.photo_urls = [
   'https://breather.imgix.net/locations/p/853-broadway-1211-2-1489605745352.jpg',
   'https://breather.imgix.net/locations/p/295madison2_1469937300009.jpg?w=355',
   'http://genylabs.typepad.com/.a/6a00d8345675df69e201b8d2b6c0cf970c-pi'
 ]
-restaurant.save
+location.save
 
 puts "create 15 fake booking"
 
@@ -506,7 +507,7 @@ puts "create 15 fake booking"
     location_id: Location.all.sample.id,
     begin: Faker::Date.backward(5),
     end: Faker::Date.forward(5),
-    status: ["Confirmed", "To be confirmed"],
+    status: "To be confirmed",
     owner_feedback: (0..5).to_a.sample,
     worker_feedback: (0..5).to_a.sample,
     worker_review: Faker::ChuckNorris.fact
