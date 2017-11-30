0bdec480839d7ae0ce171d8e402aafe85572b5f1
Richard Houet <itchy.ooc@gmail.com>
2017-11-30 21:19:43 +0000

    redis new

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-config.action_cable.url = "wss://www.tribo.space"
+config.action_cable.url = "wss://www.tribo.space/cable"
 config.action_cable.allowed_request_origins = [ "http://www.tribo.space", "https://www.tribo.space" ]
 
   # Use a different cache store in production.
