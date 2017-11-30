5911afd0770af78f207b772de049fdf571dd4d1c
Diego S. Mezquiriz <diegomezquiriz@gmail.com>
2017-11-30 15:52:34 +0000

    update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  def favorites
-    @favorites = Favorite.where("favoritor_id = ?" , current_user.id)
+
+  def favourites
+    @favourites = Favorite.where("favoritor_id = ?" , current_user.id)
   end
 
   def show
